257a90e7edb23261d7573813ab77f3badf9d9d93
vovkasWeb <vova77557@gmail.com>
2023-10-01 22:25:18 +0300

    fix: view product

--- js/scrypt.js
+++ js/scrypt.js
@@ -152,12 +152,13 @@ window.addEventListener("DOMContentLoaded", () => {
   //Используем  классы для карточек
 
   class MenuCard {
-    constructor(src, alt, title, descr, price, parentSelector) {
+    constructor(src, alt, title, descr, price, parentSelector, ...classes) {
       this.scr = src;
       this.alt = alt;
       this.title = title;
       this.descr = descr;
       this.price = price;
+      this.classes = classes;
       this.parent = document.querySelector(parentSelector);
       this.transfer = 27;
       this.changeToUAN();
@@ -167,8 +168,13 @@ window.addEventListener("DOMContentLoaded", () => {
     }
     render() {
       const element = document.createElement("div");
+      if (this.classes.length === 0) {
+        this.element = "menu__item";
+        element.classList.add(this.element);
+      } else {
+        this.classes.forEach((className) => element.classList.add(className));
+      }
       element.innerHTML = `
-        <div class="menu__item">
           <img src=${this.scr} alt=${this.alt}>
           <h3 class="menu__item-subtitle">${this.title}</h3>
           <div class="menu__item-descr">${this.descr}</div>
@@ -177,7 +183,7 @@ window.addEventListener("DOMContentLoaded", () => {
           <div class="menu__item-cost">Цена:</div>
           <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
           </div>
-        </div>
+
       `;
       this.parent.append(element);
     }
@@ -197,7 +203,9 @@ window.addEventListener("DOMContentLoaded", () => {
     'Меню "Премиум"',
     "В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!",
     14,
-    ".menu .container"
+    ".menu .container",
+    "menu__item",
+    "big"
   ).render();
 
   new MenuCard(
@@ -206,6 +214,8 @@ window.addEventListener("DOMContentLoaded", () => {
     'Меню "Постное"',
     "Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.",
     21,
-    ".menu .container"
+    ".menu .container",
+    "menu__item",
+    "big"
   ).render();
 });
