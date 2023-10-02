e1e13285cfb884375a40161ff702c423a0d471b2
vovkasWeb <vova77557@gmail.com>
2023-10-02 14:40:26 +0300

    add: aploed

--- js/scrypt.js
+++ js/scrypt.js
@@ -94,7 +94,6 @@ window.addEventListener("DOMContentLoaded", () => {
 
   const modalTrigger = document.querySelectorAll("[data-modal]");
   const modal = document.querySelector(".modal");
-  const modalCloseBtn = document.querySelector("[data-close]");
 
   function openModal() {
     modal.classList.toggle("show");
@@ -116,15 +115,15 @@ window.addEventListener("DOMContentLoaded", () => {
     document.body.style.overflow = "";
   }
 
-  modalCloseBtn.addEventListener(
-    "click",
-    closeModal
-    // modal.classList.add("hide");
-    // modal.classList.remove("show");
-  );
+  // modalCloseBtn.addEventListener(
+  //   "click",
+  //   closeModal
+  //   // modal.classList.add("hide");
+  //   // modal.classList.remove("show");
+  // );
 
   modal.addEventListener("click", (e) => {
-    if (e.target == modal) {
+    if (e.target == modal || e.target.getAttribute("data-close") == "") {
       closeModal();
     }
   });
@@ -258,15 +257,36 @@ window.addEventListener("DOMContentLoaded", () => {
       request.addEventListener("load", () => {
         if (request.status === 200) {
           console.log(request.response);
+           showThanksModal(message.success);
           statusMessage.textContent = message.success;
           form.reset();
-          setTimeout(() => {
-            statusMessage.remove();
-          }, 2000);
+          statusMessage.remove()
         } else {
-          statusMessage.textContent = message.failure;
+          showThanksModal(message.failure);
         }
       });
     });
+  }
+
+  function showThanksModal(message) {
+    const prevModalDialog = document.querySelector(".modal_dialog");
+    prevModalDialog.classList.add("hide");
+    openModal();
+    const showThanksModal = document.createElement("div");
+    thanksModal.classList.add("modal__dialog");
+    thanksModal.innerHTML = `
+    <div class="modal__content">
+    <div class="modal__close" data-close>x</div>
+    <div class="modal__title">${message}</div>
+    </div>
+    `;
+
+    document.querySelector(".modal").append(thanksModal);
+    setTimeout(() => {
+      thanksModal.remove();
+      prevModalDialog.classList.add("show");
+      prevModalDialog.classList.remove("hide");
+      closeModal(); 
+    }, 4000);
   }
 });
