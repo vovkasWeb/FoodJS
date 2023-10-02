23bd318ec70b3d73c8d6bb753fdc1d4c81731e44
vovkasWeb <vova77557@gmail.com>
2023-10-02 13:33:24 +0300

    add: post form to server

--- js/scrypt.js
+++ js/scrypt.js
@@ -129,11 +129,11 @@ window.addEventListener("DOMContentLoaded", () => {
     }
   });
 
-  document.addEventListener("keydown", () => {
-    if (e.code === "Escape" && modal.classList.contains("show")) {
-      closeModal();
-    }
-  });
+  // document.addEventListener("keydown", () => {
+  //   if (e.code === "Escape" && modal.classList.contains("show")) {
+  //     closeModal();
+  //   }
+  // });
 
   // const modalTimerId = setTimeout(openModal, 10000);
 
@@ -218,4 +218,55 @@ window.addEventListener("DOMContentLoaded", () => {
     "menu__item",
     "big"
   ).render();
+
+  //Forms
+  const forms = document.querySelectorAll("form");
+
+  const message = {
+    loading: "Загрузка",
+    success: "Cпасибо! Скоро мы с вами свяжемся",
+    failure: "Что-то пошло не так...",
+  };
+
+  forms.forEach((item) => {
+    postData(item);
+  });
+
+  function postData(form) {
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+
+      const statusMessage = document.createElement("div");
+      statusMessage.classList.add("status");
+      statusMessage.textContent = message.loading;
+      form.append(statusMessage);
+
+      const request = new XMLHttpRequest();
+      request.open("POST", "js/server.php");
+
+      request.setRequestHeader("Content-type", "application/json");
+      const formData = new FormData(form);
+
+      const object = {};
+      formData.forEach(function (value, key) {
+        object[key] = value;
+      });
+
+      const json = JSON.stringify(object);
+      request.send(json);
+
+      request.addEventListener("load", () => {
+        if (request.status === 200) {
+          console.log(request.response);
+          statusMessage.textContent = message.success;
+          form.reset();
+          setTimeout(() => {
+            statusMessage.remove();
+          }, 2000);
+        } else {
+          statusMessage.textContent = message.failure;
+        }
+      });
+    });
+  }
 });
